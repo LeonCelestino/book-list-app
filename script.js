1f0013b9b83840da90ffbddfeae52a361fa35255
unknown <LeonCelestino@outlook.com>
2022-07-27 23:29:14 -0300

    Added:showing the  missed field input function
    Added: remove book from list function

--- script.js
+++ script.js
@@ -26,55 +26,90 @@ class UI
 
     static addBookToList(book)
    {
-    const list = document.querySelector(".book-lists");
-    const row = document.createElement('tr');
+        const list = document.querySelector(".book-lists");
+        const row = document.createElement('tr');
 
-    row.innerHTML =  `     
-        <td>${book.title}</td>
-        <td>${book.author}</td>
-        <td>${book.isbn}</td>
-        <td class="table-click-buy smaller-th-td"><a href="#" class="buy"><i class="fa-solid fa-cart-shopping"></i></a></td>
-        <td class="table-click-favorite smaller-th-td"><a href="#" class="favorite"><i class="fa-solid fa-star"></i></a></td>
-        <td class="table-click-remove smaller-th-td"><a href="#" class="delete"><i class="fa-solid fa-trash-can"></i></a></td>
-        `;
+        row.innerHTML =  `     
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td class="table-click-buy smaller-th-td"><a href="#" class="buy"><i class="fa-solid fa-cart-shopping"></i></a></td>
+            <td class="table-click-favorite smaller-th-td"><a href="#" class="favorite"><i class="fa-solid fa-star"></i></a></td>
+            <td class="table-click-remove smaller-th-td remove"><a href="#" ><i class="fa-solid fa-trash-can"></i></a></td>
+            `;
 
-    list.appendChild(row);
+        list.appendChild(row);
 
    }
+
+   static clearFields()
+   {
+        document.querySelector(".title").value = "";
+        document.querySelector(".author").value = "";
+        document.querySelector(".isbn").value = "";
+        document.querySelector(".buy").value = "";
+   }
+
+   static removeBooks(el)
+   {
+        if (el.classList.contains("remove")){
+            el.parentElement.remove();
+        }
+   }
+
 }
 
 /* EVENTS */
     document.addEventListener('DOMContentLoaded', UI.displayBooks);
-    /* VALIDATING IF THERE IS SOME TEXT */
+    /* ADDING BOOKS */
    
-
-    
-
     document.querySelector("#book-form").addEventListener("submit", (e) => {
 
             e.preventDefault();
 
             /* GET INPUT VALUES */
-
             const title = document.querySelector('.title').value;
             const author = document.querySelector('.author').value;
             const isbn = document.querySelector('.isbn').value;
             const buy = document.querySelector('.buy').value;
+            const nodeLists = [title, author, isbn, buy];
+            const titleEmpty = document.querySelector('.title');
+            const authorEmpty = document.querySelector('.author');
+            const isbnEmpty = document.querySelector('.isbn');
+            const buyEmpty = document.querySelector('.buy');
+            const emptyInput = [titleEmpty, authorEmpty, isbnEmpty, buyEmpty];
             /* VALIDATING */
-
-        if (title === "" || author === "" || isbn === "" || buy === "" )
+            nodeLists.forEach((element, index) => {
+                
+                if (element === "" || element === null)
+                {
+                    console.log(index);
+                    emptyInput[index].classList.add("emptyInput");
+                }  else 
+                {
+                    emptyInput[index].classList.remove("emptyInput")
+                }
+            })
+            if (title === "" || author === "" ||  isbn ==="" ||  buy === "")
             {
                 const error = document.querySelector('p');
-                error.setAttribute('class', 'submit-message error');
-                error.innerHTML = `You need to type each information`
+                setInterval(()=>{    
+                    error.setAttribute('class', 'error');
+                    error.innerHTML = `YOU NEED TO FILL ALL FIELDS!`
+                }, 5000);
             } else
             {
-                const success = document.querySelector('p');
                 const book = new Book(title, author, isbn);
-                success.setAttribute('class', 'submit-message success');
-                success.innerHTML = `You have added somebook!`
-
+                const success = document.querySelector('p');
+                success.setAttribute('class', 'success');
+                success.innerHTML = `You have added your book!`;
                 UI.addBookToList(book);
+                UI.clearFields();
             }
         }
     )
+
+    /* REMOVING BOOKS */
+    document.querySelector(".book-lists").addEventListener('click', (e) => {
+        UI.removeBooks(e.target);
+    })
